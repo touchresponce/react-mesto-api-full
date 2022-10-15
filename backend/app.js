fd0d3cb654612f301e8eeea1efeabed1bc808426
touchresponce <touchresponce71@gmail.com>
2022-10-15 20:40:38 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const handleError = require('./middlewares/handleError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -12,6 +13,8 @@ app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use(cors);
 
 app.use(requestLogger); // логгер запроса
 
