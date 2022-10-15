343d2b406ca202dc787c1317114ab1f3fb90117c
touchresponce <touchresponce71@gmail.com>
2022-10-15 22:11:14 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -2,21 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-// const cors = require('cors');
+const cors = require('cors');
 const handleError = require('./middlewares/handleError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
+// const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use(cors);
-// app.use(cors());
+// app.use(cors);
 
 app.use(requestLogger); // логгер запроса
 
