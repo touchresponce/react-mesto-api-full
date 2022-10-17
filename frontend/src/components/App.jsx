e5efb1d65c3a2b0bddc85c4a6f7eab08eb0e1081
touchresponce <touchresponce71@gmail.com>
2022-10-17 18:32:11 +0300

    fix: email

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -204,7 +204,7 @@ export default function App() {
         .getToken(jwt)
         .then((res) => {
           setLoggedIn(true);
-          setEmail(res.email);
+          setEmail(res.user.email);
           console.log(res)
           history.push("/");
         })
