d6280a8047c4528927b00b6cb4104e7f16bb9b20
touchresponce <touchresponce71@gmail.com>
2022-10-18 14:04:06 +0300

    test likes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -69,8 +69,30 @@ export default function App() {
   }
 
   // лайки логика
+  // function handleCardLike(card) {
+  //   const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  //   if (!isLiked) {
+  //     api
+  //       .like(card._id, !isLiked)
+  //       .then((newCard) => {
+  //         setCards((state) =>
+  //           state.map((c) => (c._id === card._id ? newCard : c))
+  //         );
+  //       })
+  //       .catch((err) => console.log(err));
+  //   } else {
+  //     api
+  //       .dislike(card._id, !isLiked)
+  //       .then((newCard) => {
+  //         setCards((state) =>
+  //           state.map((c) => (c._id === card._id ? newCard : c))
+  //         );
+  //       })
+  //       .catch((err) => console.log(err));
+  //   }
+  // }
   function handleCardLike(card) {
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = card.likes.some((i) => i === currentUser._id);
     if (!isLiked) {
       api
         .like(card._id, !isLiked)
--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -13,7 +13,6 @@ export default function Card({ card, onCardClick, onCardLike, onConfirmOpen, onC
 
   // проверка лайков
   console.log(card)
-  // const isLiked = card.likes.some((i) => i._id === currentUser._id);
   const isLiked = card.likes.some((i) => i === currentUser._id);
   const cardLikeButtonClassName = `${
     isLiked ? 'element__like element__like-active' : 'element__like'
